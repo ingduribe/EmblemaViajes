3e9b026a6e33242cecf5d0dcfc54df2b60bbdf10
ingduribe <ingduribe@gmail.com>
2018-06-06 22:49:42 -0500

    modificando tamano logos

--- formularioRegistroHoteles/marcas.php
+++ formularioRegistroHoteles/marcas.php
@@ -12,7 +12,7 @@
 
       <div class="marcas">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="50%" src="./images/logo_emblema_hoteles.jpg" alt="">  
+        <img width="10%" src="./images/logo_emblema_hoteles.jpg" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Web principal de reservas de hoteles afiliados a Emblema. Venta B2C (venta directa) y B2B (venta a profesionales). Todos los mercados, categorías y segmentos. 
         </label>
@@ -20,7 +20,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./img/Logo.jpg" alt="">  
+        <img width="10%" src="./img/Logo.jpg" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Web principal de reservas de hoteles, viajes y servicios complementarios. Venta B2C (venta directa) y B2B (venta a profesionales). Todos los mercados, categorías y segmentos. 
         </label>
@@ -28,7 +28,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/logo INSIGNIA VIAJES small-lightbox.jpg" alt="">  
+        <img width="10%" src="./images/logo INSIGNIA VIAJES small-lightbox.jpg" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia DMC/Receptiva para Cartagena y Bogotá, Próximamente también Medellín, Cali, Santa Marta y Barranquilla. Venta B2C (venta directa) y B2B (venta a profesionales). Se ofrecen todos los servicios en alojamiento, transporte, actividades y complementarios en estos destinos a agencias emisoras en otras ciudades o destinos, como también a empresas y clientes directos.  
         </label>
@@ -36,7 +36,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/logo EB - small.jpg" alt="">  
+        <img width="10%" src="./images/logo EB - small.jpg" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia online de venta B2B (venta a profesionales), para reservas hoteleras con acceso exclusivo a agencias, empresas y profesionales del turismo. Todos los destinos, mercados y categorías de hotel.  
         </label>
@@ -44,7 +44,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/logo RESERVEBARATO - small.jpg" alt="">  
+        <img width="10%" src="./images/logo RESERVEBARATO - small.jpg" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia online de venta B2C (venta directa), para reservas hoteleras, servicios complementarios y actividades. Las ventas son asistidas siempre a través de un agente de reservas de Emblema online, donde asesora al cliente de las opciones más económicas. La interactuación entre cliente y asesor es directa. Principalmente mercado nacional 80%, internacional 20%. Todas las categorías de hotel.  
         </label>
@@ -53,7 +53,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/logo TODOHOTELESCOLOMBIA - small.png" alt="">  
+        <img width="10%" src="./images/logo TODOHOTELESCOLOMBIA - small.png" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia online de venta B2C (venta directa), para reservas de hoteles exclusivamente situados en Colombia. Mercado nacional 50%, internacional 50%. Todas las categorías de hotel.   
         </label>
@@ -61,7 +61,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/logo GOLF AND TRIP - small-lightbox.jpg" alt="">  
+        <img width="10%" src="./images/logo GOLF AND TRIP - small-lightbox.jpg" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia online de venta B2C (venta directa) y B2B (venta a profesionales), para reservas de hoteles especializados en el segmento Golf. Mercado nacional 50%, internacional 50%. Categorías de hotel a partir de 3 estrellas.   
         </label>
@@ -69,7 +69,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/emblema_rentals.jpg" alt="">  
+        <img width="10%" src="./images/emblema_rentals.jpg" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia online de venta B2C (venta directa) y B2B (venta a profesionales), para reservas de casas, apartamentos y otros alojamientos alternativos. Los hoteles que incluyan dentro de su estrategia de venta las reservas bajo modalidad “Full House” o reserva en exclusiva del hotel completo pueden afiliarse también a este canal. Mercado nacional 50%, internacional 50%.    
         </label>
@@ -77,7 +77,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/CASIOPEA white - small.png" alt="">  
+        <img width="10%" src="./images/CASIOPEA white - small.png" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia online de venta B2C (venta directa) y B2B (venta a profesionales), para reservas de hoteles especializados en el segmento Lujo. Mercado nacional 50%, internacional 50%. Categorías de hotel a partir de 4 estrellas superior.    
         </label>
@@ -85,7 +85,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/MOCHILATRAVEL - small.png" alt="">  
+        <img width="10%" src="./images/MOCHILATRAVEL - small.png" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia online de venta B2C (venta directa) y B2B (venta a profesionales), para reservas de hoteles especializados en el segmento mochilero. Mercado nacional 50%, internacional 50%. Categorías de hotel bajas, hostales, moteles, etc. Alternativos a este colectivo de viajero.     
         </label>
@@ -93,7 +93,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/logo mejoratuhotel - small.png" alt="">  
+        <img width="10%" src="./images/logo mejoratuhotel - small.png" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia online de venta B2C (venta directa), para reservas hoteleras. Las ventas son asistidas siempre a través de un agente de reservas de Emblema online, donde asesora al cliente de las opciones más económicas. La interactuación entre cliente y asesor es directa. Principalmente mercado nacional 80%, internacional 20%. Todas las categorías de hotel. Todas las categorías de hotel.     
         </label>
@@ -101,7 +101,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/logo TUSALONO - small.jpg" alt="">  
+        <img width="10%" src="./images/logo TUSALONO - small.jpg" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia online especializada en reserva y contratación de salones y espacios para eventos y reuniones y otros servicios complementarios. Portal indicado para hoteles con espacios, salones y venues. Mercado nacional 75%, internacional 25%, todas las categorías de hotel.      
         </label>
@@ -109,7 +109,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/logo BED & PRIDE - small.png" alt="">  
+        <img width="10%" src="./images/logo BED & PRIDE - small.png" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia online de venta B2C (venta directa) y B2B (venta a profesionales), para reservas de hoteles especializados en el segmento LGTB. Mercado nacional 20%, internacional 80%. Todas las categorías de hotel.         
         </label>
@@ -117,7 +117,7 @@
 
       <div class="marcas mt-4">
         <input class="mt-4" type="radio" class="form-control" id="emb_hoteles" >
-        <img width="20%" src="./images/logo MEJORATUVIAJE II - small.jpg" alt="">  
+        <img width="10%" src="./images/logo MEJORATUVIAJE II - small.jpg" alt="">  
         <label for="nombre contacto" class="col-md-8 col-form-label">
           Agencia de viajes online de venta B2C (venta directa), para reservas hoteleras y otros servicios turísticos. Principalmente mercado nacional 80%, internacional 20%. Todas las categorías de hotel.         
         </label>
