e1417c53a13e76b174adf06c492f3ddc738d66e1
ingduribe <ingduribe@gmail.com>
2018-06-10 17:56:29 -0500

    primer pdf

---
+++ formularioRegistroAgencias/terminos_cond.php
@@ -0,0 +1,11 @@
+<?php 
+
+require __DIR__.'./../vendor/autoload.php';
+
+use Spipu\Html2Pdf\Html2Pdf;
+
+$html2pf = new Html2Pdf();
+$html2pf->writeHTML('<h1>Terminos y condiciones</h1>');
+$html2pf->output("Terminos y condiciones.pdf");
+
+?>
